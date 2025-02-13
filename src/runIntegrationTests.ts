5045c8d5b6965ce25af212782122df918db43623
roboteng <roboteng@gmail.com>
2025-02-13 13:52:06 -0700

    patch: added delete, get, create tests

--- src/runIntegrationTests.ts
+++ src/runIntegrationTests.ts
@@ -1,20 +1,50 @@
 import Executions from './Executions'
+import PipelineImpl from './Pipeline'
 
-async function testAbleToRun() {}
+const defaultPipelinePath =
+    'C:\\Users\\suppo\\Lumena\\NeuropypeFiles\\lumena-edge-pipelines\\pipelines\\quality_check.pyp'
 
 async function testGetAllExecutions() {
     const executionIds = await Executions.getAll()
 
-    if (executionIds.length === 0) {
-        throw new Error('No executions found')
+    if (executionIds.length !== 0) {
+        throw new Error("Found executions, but didn't expect to")
+    }
+}
+
+async function testCreatePipeline() {
+    await PipelineImpl.Pipeline({ path: defaultPipelinePath })
+    const executionIds = await Executions.getAll()
+
+    if (executionIds.length !== 1) {
+        throw new Error("Should have found 1 execution, but didn't")
+    }
+}
+
+async function testCreateThenDeleteThenGetAll() {
+    for (let i = 0; i < 3; i++) {
+        await PipelineImpl.Pipeline({ path: defaultPipelinePath })
+    }
+    await Executions.deleteAll()
+    const executions = await Executions.getAll()
+
+    if (executions.length !== 0) {
+        throw new Error('Not all executions were deleted, but should have been')
     }
 }
 
 async function main() {
-    process.env.NEUROPYPE_BASE_URL = 'http://localhost:6937/'
+    process.env.NEUROPYPE_BASE_URL = 'http://192.168.8.11:6937/'
 
-    await testAbleToRun()
-    await testGetAllExecutions()
+    const tests = [
+        testCreateThenDeleteThenGetAll,
+        testGetAllExecutions,
+        testCreatePipeline,
+    ] as const
+    for (const test of tests) {
+        await Executions.deleteAll()
+        await test()
+    }
 }
 
 main().catch((err) => {
