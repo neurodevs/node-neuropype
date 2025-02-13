f442a0574fb1cd90d3638d230ac9ebd1483248ce
ericthecurious <hello@ericthecurious.com>
2025-02-13 17:08:48 -0600

    patch: update integration tests (with @roboteng)

--- src/runIntegrationTests.ts
+++ src/runIntegrationTests.ts
@@ -1,23 +1,26 @@
+import { generateId } from '@sprucelabs/test-utils'
 import Executions from './components/Executions'
 import PipelineImpl from './components/Pipeline'
 
-const defaultPipelinePath =
-    'C:\\Users\\suppo\\Lumena\\NeuropypeFiles\\lumena-edge-pipelines\\pipelines\\quality_check.pyp'
+async function beforeEach() {
+    await Executions.deleteAll()
+}
 
 async function testGetAllExecutions() {
-    const executionIds = await Executions.getAll()
+    const numExecutions = await getNumExecutions()
 
-    if (executionIds.length !== 0) {
-        throw new Error("Found executions, but didn't expect to")
+    if (numExecutions !== 0) {
+        throw new Error(`Should have found 0 executions, not ${numExecutions}!`)
     }
 }
 
 async function testCreatePipeline() {
     await createPipeline()
-    const executionIds = await Executions.getAll()
 
-    if (executionIds.length !== 1) {
-        throw new Error("Should have found 1 execution, but didn't")
+    const numExecutions = await getNumExecutions()
+
+    if (numExecutions !== 1) {
+        throw new Error(`Should have found 1 execution, not ${numExecutions}!`)
     }
 }
 
@@ -26,10 +29,11 @@ async function testCreateThenDeleteThenGetAll() {
         await createPipeline()
     }
     await Executions.deleteAll()
-    const executions = await Executions.getAll()
 
-    if (executions.length !== 0) {
-        throw new Error('Not all executions were deleted, but should have been')
+    const numExecutions = await getNumExecutions()
+
+    if (numExecutions !== 0) {
+        throw new Error(`Should have found 0 executions, not ${numExecutions}!`)
     }
 }
 
@@ -37,13 +41,61 @@ async function testGetDetails() {
     const pipeline = await createPipeline()
     const details = await pipeline.getDetails()
 
-    if (!details) {
-        throw new Error('No details found!')
+    const actual = details.graph.description.name
+
+    if (actual !== expectedName) {
+        throw new Error(`Name should be ${expectedName}, not ${actual}!`)
     }
 }
 
+async function testStartAndStopPipeline() {
+    const pipeline = await createPipeline()
+    let details = await pipeline.getDetails()
+
+    if (details.state.running) {
+        throw new Error('Pipeline should not be running at start!')
+    }
+
+    await pipeline.start()
+    details = await pipeline.getDetails()
+
+    if (!details.state.running) {
+        throw new Error('Pipeline should be running after start!')
+    }
+
+    await pipeline.stop()
+    details = await pipeline.getDetails()
+
+    if (details.state.running) {
+        throw new Error('Pipeline should not be running after stop!')
+    }
+}
+
+async function testUpdateParameters() {
+    const hostname = generateId()
+    const pipeline = await createPipeline()
+    await pipeline.update({ hostname })
+
+    const details = await pipeline.getDetails()
+
+    const matchingNodes = details.graph.nodes.filter((node) =>
+        node.parameters.some((param) => param.value === hostname)
+    )
+
+    const numMatchingNodes = matchingNodes.length
+
+    if (numMatchingNodes !== 1) {
+        throw new Error(`Should have found 1 node, not ${numMatchingNodes}!`)
+    }
+}
+
+async function getNumExecutions() {
+    const executionIds = await Executions.getAll()
+    return executionIds.length
+}
+
 async function createPipeline() {
-    return await PipelineImpl.Create(defaultPipelinePath)
+    return await PipelineImpl.Create(pypFilepath)
 }
 
 async function main() {
@@ -54,12 +106,21 @@ async function main() {
         testGetAllExecutions,
         testCreatePipeline,
         testGetDetails,
+        testStartAndStopPipeline,
+        testUpdateParameters,
     ] as const
+
     for (const test of tests) {
-        await Executions.deleteAll()
+        console.log(`Running test: ${test}...`)
+        await beforeEach()
         await test()
     }
 }
+
+const pypFilepath =
+    'C:\\Users\\suppo\\Lumena\\NeuropypeFiles\\lumena-edge-pipelines\\pipelines\\quality_check.pyp'
+
+const expectedName = 'Quality Check Muse S (2nd generation) 1.0'
 
 main().catch((err) => {
     console.error(err)
