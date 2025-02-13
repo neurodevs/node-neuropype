33a8e4f8e14531c5bca15a66928388e93941c6eb
ericthecurious <hello@ericthecurious.com>
2025-02-13 14:08:23 -0600

    minor: create integration tests for neuropype

--- src/runIntegrationTests.ts
+++ src/runIntegrationTests.ts
@@ -1,4 +1,21 @@
-async function main() {}
+import Executions from './Executions'
+
+async function testAbleToRun() {}
+
+async function testGetAllExecutions() {
+    const executionIds = await Executions.getAll()
+
+    if (executionIds.length === 0) {
+        throw new Error('No executions found')
+    }
+}
+
+async function main() {
+    process.env.NEUROPYPE_BASE_URL = 'http://localhost:6937/'
+
+    await testAbleToRun()
+    await testGetAllExecutions()
+}
 
 main().catch((err) => {
     console.error(err)
